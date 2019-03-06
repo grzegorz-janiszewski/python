3a33878d9ae9cc87e4e50f2c4b5814b9f1dc00b8
Grzegorz <kotemt@gmail.com>
2019-03-06 15:58:43 +0100

    03c

--- python01/7.py
+++ python01/7.py
@@ -1,3 +1,9 @@
+import xml.etree.ElementTree as ET
+from _elementtree import Element
+from typing import Union
+from xml.etree.ElementTree import Element
+
+
 class Pracownik:
     def __init__(self, imie, nazwisko, stanowisko, pensja):
         self.imie = imie
@@ -19,8 +25,8 @@ class Firma:
         return res
 
 
-adam = Pracownik('Adam', 'Kowalski', 'kierownik', 2000)
-karol = Pracownik('Karol', 'Iksinski', 'Wyrobnik ciasteczek', 300)
+adam = Pracownik('Adam', 'Kowalski', 'kierownik', "2000")
+karol = Pracownik('Karol', 'Iksinski', 'Wyrobnik ciasteczek', "300")
 
 korporacja_ciastkarska = Firma()
 
@@ -31,3 +37,18 @@ print(korporacja_ciastkarska.listapracownikow())
 
 korporacja_ciastkarska.zwolnij(adam)
 print(korporacja_ciastkarska.listapracownikow())
+
+element = ET.Element('pracownicy')
+pracownik1 = ET.SubElement(element, 'adam', {"imie" : adam.imie, "nazwisko": adam.nazwisko, "stanowisko": adam.stanowisko, "pensja": adam.pensja} )
+pracownik2 = ET.SubElement(element, 'karol', {"imie" : karol.imie, "nazwisko": karol.nazwisko, "stanowisko": karol.stanowisko, "pensja": karol.pensja})
+
+tree = ET.ElementTree(element)
+tree.write("pracownicy.xml")
+
+zpliku = ET.parse("pracownicy.xml")
+root = zpliku.getroot()
+
+
+print("Dane odczytane z XML")
+for x in root:
+    print(x.attrib['imie'], x.attrib['nazwisko'])
