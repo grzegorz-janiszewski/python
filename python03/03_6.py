28df45095fd146a4ad5032b80f00280607c6bd25
Grzegorz <kotemt@gmail.com>
2019-03-06 13:01:26 +0100

    03c

--- python03/03_6.py
+++ python03/03_6.py
@@ -6,17 +6,19 @@ color = {'normal': '\x1b[0m',
          'greenbg': '\x1b[6;30;42m',
          }
 
-RE_ABC = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+#RE_ABC = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+RE_ABC = re.compile(r'((1\d{2})|(2[0-5]{2})|([1-9]{1,2}))\.((1\d{2})|(2[0-5]{2})|([1-9]{1,2}))\.((1\d{2})|(2[0-5]{2})|([1-9]{1,2}))\.((1\d{2})|(2[0-5]{2})|([1-9]{1,2}))')
 
 tests = '''\
 to nie jest python Szkolenie
 123
+250.23.254.2
 10.10.1.12
 192.168.1.23
 moze jednak jest
 129.34
 1227.0.0.1
-127.0.0.1 oraz 192.168.1.42
+827.0.0.1 oraz 192.168.1.42
 jednak TO JUZ NIE
 Python to nie jest Szkolenie
 '''
